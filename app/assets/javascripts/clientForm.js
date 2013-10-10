861b0a982cdb7a36b66cc4755ee987b67b336d43
coolhhc <coolhhc@gmail.com>
2013-10-10 19:04:30 +1100

    Fixed nested seachCondition, work on base case and answers (stop the nesting) !

--- app/assets/javascripts/clientForm.js
+++ app/assets/javascripts/clientForm.js
@@ -18,46 +18,117 @@ function getRules(){
 };
 
 function buildForm(rulesObj){
-    var count = 1;
+    var ruleCount = 1;
     for( q in rulesObj ){
-        newQuestion(org, "r" + count , rulesObj[q].condition);
-        count++;
+        newQuestion(org, "r" + ruleCount, rulesObj);
+        ruleCount++;
     }
 
 };
 
-function newQuestion(appendTo, liName, question){
+function newQuestion(appendTo, liName, rulesObj){
     var li = liTag(liName);
-    questionContext(li, question, liName);
+    questionContext(li, liName, rulesObj);
     appendTo.appendChild(li);
 };
 
 
-function questionContext(appendTo, question, pastName){
-    appendTo.appendChild(labelTag(question));
-    appendTo.appendChild(radioTag(pastName, pastName + "t1"));
+function questionContext(appendTo, pastName, rulesObj){
+    //alert(pastName);
+    var level = getLevel(pastName);
+    var true_value = pastName + "t" + (parseInt(level) + 1);
+    var false_value = pastName + "f" + (parseInt(level) + 1);
+    //alert(value);
+
+    appendTo.appendChild(labelTag(findCondition(pastName+"c", rulesObj)));
+    appendTo.appendChild(radioTag(pastName, true_value, rulesObj));
     appendTo.appendChild(labelTag("true"));
-    appendTo.appendChild(radioTag(pastName, pastName + "f1"));
+    appendTo.appendChild(radioTag(pastName, false_value, rulesObj));
     appendTo.appendChild(labelTag("false"));
     appendTo.appendChild(ulTag(pastName + "u"));
 };
 
-function radioTag(pastName, value){
+
+function findCondition(path, rulesObj){
+    //alert("path: "+path+"\nrulesObj: "+rulesObj);
+
+    var array = path.match(/([rtfc][1-9]*)/g);
+    var ruleNum = array.shift();
+    var num = ruleNum.match(/[1-9]+/);
+    var index = parseInt(num)-1;
+    var ruleObj = rulesObj[index];
+
+    var noNumberArray = stripNumbers(array);
+
+    var string = searchCondition(ruleObj, noNumberArray);
+    //alert("returns: "+string);
+
+    return string;
+};
+
+
+function searchCondition(rule, array){
+    //alert("ruleObj IN: " + target);
+    var action = array.shift();
+    //alert("action: "+action+"\narray: "+array);
+
+    if(action == "t"){
+        //alert("rule.true_path: "+rule.true_path);
+        return searchCondition(rule.true_path, array);
+    }
+    else if(action == "f"){
+        //alert("rule.false_path: "+rule.false_path);
+        return searchCondition(rule.false_path, array);
+
+    }
+    else if(action == "c"){
+        //alert("rule.condition: "+rule.condition);
+        return rule.condition;
+    }
+    else{
+        throw {
+            name: 'NoCondition',
+            message: "Can't find condition with this path"
+        }
+    }
+};
+
+
+function stripNumbers(array){
+    var noNumberArray = [];
+    for(i in array){
+       noNumberArray.push(array[i].match(/[tfc]/));
+    }
+    return noNumberArray;
+};
+
+function getLevel(path){
+    var r = /^[r][1-9]+$/;
+    var start = r.test(path);
+    if(start){
+        return 0;
+    }
+    else{
+        var regex = /[1-9]+$/;
+        var level = regex.exec(path);
+        return level[0];
+    }
+};
+
+
+function radioTag(pastName, value, rulesObj){
     var radioInput = document.createElement('input');
     radioInput.setAttribute('type', 'radio');
     radioInput.setAttribute('name', pastName);
     radioInput.setAttribute('value', value);
 
-    var nestedQuestion = 'newQuestion(' + pastName + 'u, "' + value + '", "' + getNestedQuestion(value) + '")'
-    // stop when reached leaf
-    radioInput.setAttribute('onclick', nestedQuestion);
-    //radioInput.onclick = function() {nestedQuestion};  // for IE
+    radioInput.onclick = (function() {
+                              return function(){
+                                  var ul = document.getElementById(pastName+"u");
+                                  newQuestion(ul, value, rulesObj);
+                              }
+                         })();
     return radioInput;
-};
-
-function getNestedQuestion(value){
-   //make ajax call!!!!!
-    return ""+value+"";
 };
 
 function labelTag(name){
--- app/helpers/path_helper.rb
+++ app/helpers/path_helper.rb
@@ -1,14 +1,6 @@
 module PathHelper
     def helper_link rules
 
-        #count = 0
-        #inside = rules.inject("") do |rule_calls, rule|
-        #    count = count + 1
-        #    question = rule.condition
-        #    rule_calls + "newQuestion(org, \"r" + count.to_s + "\", \"" + question + "\");"
-        #end
-
-        #getRules = "getRules();"
         makeForm = "getRules();"
         script = "window.onload = function(){" + makeForm + "};"
 
